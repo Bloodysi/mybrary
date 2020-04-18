a45cc3ccef8d3c6ad1db5828fb116fe65f5e8890
bloodysi <buenoct2001@gmail.com>
2020-04-18 16:10:57 -0400

    initial setup

---
+++ index.js
@@ -0,0 +1,37 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+//EXPRESS CONTENT
+const express = require('express')
+const ejsLayouts = require('express-ejs-layouts')
+const app = express()
+
+//OTHER CONTENT
+
+
+//ROUTES PATH
+const indexRouter = require('./routes/index')
+
+//DB
+require('./db')
+
+//SETTIGNS
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layouts', 'layouts/layout')
+
+//MIDDLEAWARES
+app.use(ejsLayouts)
+app.use(express.urlencoded({extended: false, limit: '10mb'}))
+
+//ROUTES
+app.use('/', indexRouter)
+
+//STATIC FILES
+app.use(express.static('public'))
+
+//ON!
+app.listen(3000, ()=>{
+    console.log('server on!')
+})
