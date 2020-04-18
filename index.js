c85f22fd987466029ac943a58c3359e28546081d
bloodysi <buenoct2001@gmail.com>
2020-04-18 17:11:39 -0400

    initial author routes

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const app = express()
 
 //ROUTES PATH
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 //DB
 require('./db')
@@ -27,6 +28,7 @@ app.use(express.urlencoded({extended: false, limit: '10mb'}))
 
 //ROUTES
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 //STATIC FILES
 app.use(express.static('public'))
---
+++ routes/authors.js
@@ -0,0 +1,50 @@
+const {Router} = require('express')
+const router = Router()
+
+const Author = require('../models/authors')
+
+//ALL AUTHORS ROUTE
+router.get('/', async (req, res)=>{
+    let searchOption = {}
+
+    if (req.query.name != null && req.query.name !== ''){
+        searchOption.name = new RegExp(req.query.name, 'i')        
+    }    
+
+    try {
+        const authors = await Author.find(searchOption)
+        res.render('authors/index', {
+            authors: authors,
+            searchOption: req.query
+        })
+    } catch {
+        res.redirect('/')
+    }
+})
+
+//NEW AUTHOR ROUTE
+
+router.get('/new', (req, res)=>{
+    res.render('authors/new', {author: new Author()})
+})
+
+//CREATE AUTHOR ROUTE
+
+router.post('/',async (req, res)=>{
+    const author = new Author({
+        name: req.body.name
+    })
+    try {        
+        const newAuthor = await author.save()
+        res.redirect('authors')
+
+    } catch (error) {
+        console.error(error)
+        res.render('authors/new',{
+            author: author,
+            errorMessage: 'Error creating author'
+        })
+    }
+})
+
+module.exports = router
